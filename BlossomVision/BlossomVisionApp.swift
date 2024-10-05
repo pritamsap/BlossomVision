a81c779642d723c7319ce65e424f2169271633b8
pritamsap <pritamsap2@gmail.com>
2024-10-04 20:34:05 -0400

    Initial Commit

---
+++ BlossomVision/BlossomVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomVisionApp.swift
+//  BlossomVision
+//
+//  Created by pritam on 2024-10-04.
+//
+
+import SwiftUI
+
+@main
+struct BlossomVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlossomVision/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlossomVision
+//
+//  Created by pritam on 2024-10-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
